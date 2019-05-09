6b265f6efd788a694c125684efec7b35b714dd1b
Ian McNally <ianmcnally@users.noreply.github.com>
2019-05-09 17:57:43 -0400

    Add back prismjs for code highlighting

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,6 +28,9 @@ module.exports = {
       options: {
         plugins: [
           {
+            resolve: 'gatsby-remark-prismjs'
+          },
+          {
             resolve: 'gatsby-remark-images',
             options: {
               maxWidth: 590,
